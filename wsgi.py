e48226307fb6faf55ad534bdb7b6156ab6ac7571
Sashka <krasnshhk@gmail.com>
2025-07-05 19:05:44 +0200

    changed port for app

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app import app  # Assuming `app` is the Flask instance in your app.py file
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__ == '__main__':
+    app.run(port=5001)  # or any other available port
